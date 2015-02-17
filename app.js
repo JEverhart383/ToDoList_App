d0f0f0bff094f27c96ca8587bdd9f1bc870fe5a3
Jeff Everhart <Jeffeverhart383@gmail.com>
2015-02-17 14:57:50 -0500

    add completed dates, clean ui

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-
-});//End Remove button $("document").ready(function(){
+$("document").ready(function(){
 
 
 //Define globals
@@ -8,10 +6,14 @@ var newTask_array = [];
 var doneTask_array = [];
 var deletedTask_array = [];
 
-function createNewTask(name, date, importance){
+console.log(deletedTask_array);
+
+
+function createNewTask(name, date, importance, completeDate){
 	this.taskName = name;
 	this.taskDate = date;
 	this.taskImport = importance;
+	this.completeDate = completeDate;
 }
 
 function writeList(listArray, domElement){
@@ -69,6 +71,8 @@ function writeList(listArray, domElement){
 	//functionto write newTask_array to local storage
 	//localStorage.setItem('newTask_array', JSON.stringify(listArray)); 
 
+	console.log(deletedTask_array);
+
 } //End writeList function
 
 function writeDoneList(listArray, domElement){
@@ -90,14 +94,11 @@ function writeDoneList(listArray, domElement){
 		} else {
 			importSymbol = "<span></span>";
 		}
-
-			//Remove double quotes and add class and styling to LIs
-
-			//var done_string = "<li>" + listArray[i].taskName + " " + listArray[i].taskDate + " " + importSymbol + " " + "<button class='remove btn btn-danger'><span class='glyphicon glyphicon-remove'></span>  Remove</button></li>";
 			
 			var done_string = "<li><div class='panel panel-default'>";
 			done_string += "<div class='panel-heading'><h4 class='panel-title'><s>" + listArray[i].taskName + " " + importSymbol + "</s></h4></div>";
 			done_string += "<div class='panel-body'>" + "<span class='glyphicon glyphicon-calendar'></span>" + " " + listArray[i].taskDate;
+			done_string += "</br> <span class='glyphicon glyphicon-ok'></span>&nbsp;Date Completed: " + listArray[i].completeDate;
 			done_string += "</br></br><button class='remove btn btn-danger'><span class='glyphicon glyphicon-remove'></span> Remove</button>";
 			done_string +=" </div></li>";
 
@@ -113,6 +114,8 @@ function writeDoneList(listArray, domElement){
 	$("input[type=checkbox]").attr("checked", false);
 	
 	var todo_list = domElement.html();
+
+	console.log(deletedTask_array);
 	
 	//functionto write newTask_array to local storage
 	//localStorage.setItem('doneTask_array', JSON.stringify(listArray)); 
@@ -126,7 +129,6 @@ function writeDeletedList(listArray, domElement){
 	domElement.children().remove();
 
 	var importSymbol; 
-	var panelClass = "panel-default";
 
 	for (i=0; i < listArray.length; i++){
 
@@ -137,40 +139,49 @@ function writeDeletedList(listArray, domElement){
 			importSymbol = "<span></span>";
 		}
 
-		//Remove double quotes and add class and styling to LIs
-
-		var todo_string = "<li><div class='panel " + panelClass + "'>";
+		var todo_string = "<li><div class='panel panel-default'>";
 		todo_string += "<div class='panel-heading'><h4 class='panel-title'>" + listArray[i].taskName + " " + importSymbol + "</h4></div>";
 		todo_string += "<div class='panel-body'>" + "<span class='glyphicon glyphicon-calendar'></span>" + " " + listArray[i].taskDate;
-		todo_string += "</br></br><button class='done_btn btn btn-default'><span class='glyphicon glyphicon-refresh'></span> DONE</button>" + "      ";
-		todo_string += "<button class='done_btn btn btn-danger'><span class='glyphicon glyphicon-trash'></span> DONE</button>";
+		todo_string += "</br> <span class='glyphicon glyphicon-ok'></span>&nbsp;Date Completed: " + listArray[i].completeDate;
+		todo_string += "</br></br><button class='restore_btn btn btn-default'><span class='glyphicon glyphicon-refresh'></span> Restore</button>" + "      ";
+		todo_string += "<button class='trash_btn btn btn-danger'><span class='glyphicon glyphicon-trash'></span> Trash</button>";
 		todo_string +=" </div></li>";
 		
 		domElement.append(todo_string); 
 
 	}//End for loop 
 
+	console.log(deletedTask_array);
+
 } //End writeDeletedList function
 
 
 //If statement to load newTask_array from localStorage and write to todoList
 
-if (localStorage.getItem("newTask_array")) {
+	if (localStorage.getItem("newTask_array")) {
 
-		newTask_array = JSON.parse(localStorage.getItem("newTask_array"));
+			newTask_array = JSON.parse(localStorage.getItem("newTask_array"));
 
-		writeList(newTask_array, $(".todo_list"));
+			writeList(newTask_array, $(".todo_list"));
+
+			console.log(deletedTask_array);
 
 
-}//End load LocalStorage if statement 
+		}//End load LocalStorage if statement 
 
-if (localStorage.getItem("doneTask_array")){
+	if (localStorage.getItem("doneTask_array")){
 
-		doneTask_array = JSON.parse(localStorage.getItem("doneTask_array"));
+			doneTask_array = JSON.parse(localStorage.getItem("doneTask_array"));
 
-		writeDoneList(doneTask_array, $(".done_list"));
+			writeDoneList(doneTask_array, $(".done_list"));
 
-}//End load LocalStorage if statement 
+		}//End load LocalStorage if statement 
+
+	if (localStorage.getItem("deletedTask_array")){
+		deletedTask_array = JSON.parse(localStorage.getItem("deletedTask_array"));
+	} else {
+		localStorage.setItem("deletedTask_array", JSON.stringify(deletedTask_array));
+	}	
 
 //Collect all input values and attach to object, then push to newTask_array
 $(".add_task").click(function(){
@@ -201,6 +212,8 @@ $(".add_task").click(function(){
 
 	//functionto write newTask_array to local storage
 	localStorage.setItem('newTask_array', JSON.stringify(newTask_array)); 
+
+	console.log(deletedTask_array);
 	
 });//End on click function for add button 	
 
@@ -213,10 +226,58 @@ $(".todo_list").on('click','.done_btn', function (){
 	var list_index = $(this).parents("li").index();
 	var doneTask = newTask_array[list_index];
 
+
+
+	var dateConstructor = new Date();
+
+	var monthCompleted; 
+	var monthConstructor = dateConstructor.getMonth();
+
+	if (monthConstructor == 0){
+		monthCompleted = "January";
+	} 
+	if (monthConstructor == 1){
+		monthCompleted = "February";
+	}
+	if (monthConstructor == 2){
+		monthCompleted = "March";
+	}
+	if (monthConstructor == 3){
+		monthCompleted = "April";
+	}
+	if (monthConstructor == 4){
+		monthCompleted = "May";
+	}
+	if (monthConstructor == 5){
+		monthCompleted = "June";
+	}
+	if (monthConstructor == 6){
+		monthCompleted = "July";
+	}
+	if (monthConstructor == 7){
+		monthCompleted = "August";
+	}
+	if (monthConstructor == 8){
+		monthCompleted = "September";
+	}
+	if (monthConstructor == 9){
+		monthCompleted = "October";
+	}
+	if (monthConstructor == 10){
+		monthCompleted = "November";
+	}
+	if (monthConstructor == 11){
+		monthCompleted = "December";
+	}
+
+	var dateCompleted = monthCompleted + " " + dateConstructor.getDate() + ", " + dateConstructor.getFullYear();
+
+	doneTask.completeDate = dateCompleted;
+
 	doneTask_array.push(doneTask);
 	newTask_array.splice(list_index, 1);
 
-	//clear .todo_list, resort newTask_array, print out newly sorted newTask_array 
+	//resort newTask_array
 
 	newTask_array = newTask_array.sort(function(a , b){
 
@@ -242,6 +303,8 @@ $(".todo_list").on('click','.done_btn', function (){
 	//functionto write newTask_array to local storage
 	localStorage.setItem('doneTask_array', JSON.stringify(doneTask_array)); 
 
+	console.log(deletedTask_array)
+
 
 });// End on click for DONE button
 
@@ -249,11 +312,12 @@ $(".todo_list").on('click','.done_btn', function (){
 //BEGIN REMOVE BUTTON
 
 $(".done_list").on('click','.remove', function (){
+	console.log(deletedTask_array);
 
 	//get index of clicked item, remove from doneTask_array, rewrite doneTask_array
 	var list_index = $(this).parents("li").index();
-	var deletedTask = doneTask_array[list_index];
-	deletedTask_array.push(deletedTask);
+	var deleteTask = doneTask_array[list_index];
+	deletedTask_array.push(deleteTask);
 	doneTask_array.splice(list_index, 1);
 
 	localStorage.setItem('deletedTask_array', JSON.stringify(deletedTask_array));
@@ -276,11 +340,63 @@ $(".done_list").on('click','.remove', function (){
 
 });//End Remove button 
 
+
+///START SDK for deleted.html
+
 if ($(".deleted_items")){
 
 	deletedTask_array = JSON.parse(localStorage.getItem('deletedTask_array'));
 
 	writeDeletedList(deletedTask_array,$(".deleted_items"));
+
 }
+	//Start on click for restore button
+	/*$(".deleted_items").on('click','.restore_btn', function(){
+		var list_index = $(this).parent('li').index();
+
+		restoredTask = deletedTask_array[list_index];
+
+		newTask_array.push(restoredTask);
+
+		newTask_array = newTask_array.sort(function(a , b){
+
+			var dateA = new Date(a.taskDate); 
+			var dateB = new Date(b.taskDate);
+
+			if (+dateA === +dateB){
+
+				var importA = a.taskImport
+				var importB = b.taskImport
+				return importB - importA; 
+			}
+
+			return dateA - dateB;
+		}); //End sort function
+
+		deletedTask_array.splice(list_index, 1);
+
+
+		writeDeletedList(deletedTask_array, $(".deleted_items"));
+
+		localStorage.setItem('deletedTask_array', JSON.stringify(deletedTask_array));
+		localStorage.setItem('newTask_array', JSON.stringify(newTask_array));
+
+	}); //End restore on click call back 
+
+	//Start Trash on click 
+
+	$('.deleted_items').on('click', '.trash_btn', function(){
+		var list_index = $(this).parent('li').index();
+
+		deletedTask_array.splice(list_index, 1);
+
+
+		writeDeletedList(deletedTask_array, $(".deleted_items"));
+		localStorage.setItem('deletedTask_array', JSON.stringify(deletedTask_array));
+
+
+	});//End trash on click callback
+}//End deleted items if */
+
 
 });//End document ready  
